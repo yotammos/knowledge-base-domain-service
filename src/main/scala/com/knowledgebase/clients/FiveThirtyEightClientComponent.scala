1e63632cdb118bfafb6b9f92cff28c6d91aa555c
Yotam Mosinzon <yotam.mos@gmail.com>
2019-12-29 16:25:50 -0500

    parsing csv correctly

--- src/main/scala/com/knowledgebase/clients/FiveThirtyEightClientComponent.scala
+++ src/main/scala/com/knowledgebase/clients/FiveThirtyEightClientComponent.scala
@@ -1,6 +1,6 @@
 package com.knowledgebase.clients
 
-import com.knowledgebase.thrift.PollResource
+import com.knowledgebase.thrift.{PollEntry, PollResource}
 import com.knowledgebase.utils.Defaults.{FTE_HOST, FTE_PORT}
 import com.twitter.finagle.{Http, Service}
 import com.twitter.finagle.http.{Method, Request, Response, Status}
@@ -39,10 +39,34 @@ trait FiveThirtyEightClientComponent {
         .values
         .toSeq
 
-      rawPolls map buildPollResource
+      rawPolls.map(rawPoll => buildPollResource(rawPoll, colMap))
     }
 
-    def buildPollResource(rawResource: Array[Array[String]]): PollResource = ???
+    def buildPollResource(rawResource: Array[Array[String]], colMap: ColMap): PollResource = {
+      val firstLine = rawResource.head
+      PollResource(
+        cycle = firstLine(colMap.cycle).toInt,
+        pollster = firstLine(colMap.pollster),
+        fteGrade = firstLine(colMap.fteGrade),
+        sampleSize = firstLine(colMap.sampleSize).toInt,
+        officeType = firstLine(colMap.officeType),
+        startDate = firstLine(colMap.startDate),
+        endDate = firstLine(colMap.endDate),
+        stage = firstLine(colMap.stage),
+        state = {
+          val maybeState = firstLine(colMap.state)
+          if (maybeState.nonEmpty)
+            Option(maybeState)
+          else
+            None
+        },
+        entries = rawResource.map(line => PollEntry(
+          party = line(colMap.party),
+          candidate = line(colMap.candidate),
+          percentage = line(colMap.percentage).toDouble
+        ))
+      )
+    }
 
     case class ColMap(cycle: Int, pollster: Int, fteGrade: Int, sampleSize: Int, officeType: Int, startDate: Int, endDate: Int, stage: Int, party: Int, candidate: Int, percentage: Int, state: Int, questionId: Int)
 
